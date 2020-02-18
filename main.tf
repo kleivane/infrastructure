87174f171dba7b27c60b2d188b78c9125e1837bd
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-18 08:26:38 +0100

    Legg til hosted zone og domene

--- main.tf
+++ main.tf
@@ -4,5 +4,23 @@ provider "aws" {
 }
 
 resource "aws_route53_zone" "primary" {
-  name = "tinekleivane.bekk.no"
+  name = "skysett.net."
+  comment = "HostedZone created by Route53 Registrar, now managed by Terraform"
+}
+
+# Se http://www.dns-sd.org./trailingdotsindomainnames.html for
+# trailing dots forklaring
+
+resource "aws_route53_record" "primary" {
+  name            = "skysett.net."
+  ttl             = 172800
+  type            = "NS"
+  zone_id         = aws_route53_zone.primary.zone_id
+
+  records = [
+    "${aws_route53_zone.primary.name_servers.0}.",
+    "${aws_route53_zone.primary.name_servers.1}.",
+    "${aws_route53_zone.primary.name_servers.2}.",
+    "${aws_route53_zone.primary.name_servers.3}.",
+  ]
 }
