1aaf5573a05e88b148f3eea0b836beb11c0981a1
Tine Kleivane <tine.kleivane@bekk.no>
2020-03-09 11:03:51 +0100

    Kommenter ut ns
    Forsøksvis erstattet med delegation set

--- main.tf
+++ main.tf
@@ -1,26 +1,42 @@
 provider "aws" {
-  region = "eu-north-1"
+  region  = "eu-north-1"
   version = "~> 2.47"
 }
 
+resource "aws_route53_delegation_set" "primary" {
+  reference_name = "Route 53"
+}
+
 resource "aws_route53_zone" "primary" {
-  name = "skysett.net."
+  name    = "skysett.net."
   comment = "HostedZone created by Route53 Registrar, now managed by Terraform"
 }
 
 # Se http://www.dns-sd.org./trailingdotsindomainnames.html for
 # trailing dots forklaring
 
-resource "aws_route53_record" "primary" {
-  name            = "skysett.net."
-  ttl             = 172800
-  type            = "NS"
-  zone_id         = aws_route53_zone.primary.zone_id
+# resource "aws_route53_record" "primary" {
+#   name    = "skysett.net."
+#   ttl     = 172800
+#   type    = "NS"
+#   zone_id = aws_route53_zone.primary.zone_id
+#
+#   records = [
+#     "${aws_route53_zone.primary.name_servers.0}.",
+#     "${aws_route53_zone.primary.name_servers.1}.",
+#     "${aws_route53_zone.primary.name_servers.2}.",
+#     "${aws_route53_zone.primary.name_servers.3}.",
+#   ]
+# }
 
-  records = [
-    "${aws_route53_zone.primary.name_servers.0}.",
-    "${aws_route53_zone.primary.name_servers.1}.",
-    "${aws_route53_zone.primary.name_servers.2}.",
-    "${aws_route53_zone.primary.name_servers.3}.",
-  ]
-}
+
+# resource "aws_route53_record" "soa" {
+#   name            = "skysett.net."
+#   ttl             = 900
+#   type            = "SOA"
+#   zone_id         = aws_route53_zone.primary.zone_id
+#
+#   records = [
+#
+#   ]
+# }
